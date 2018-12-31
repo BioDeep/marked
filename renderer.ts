1fb6af3b7e64987c587d1b65def9a7b7112c5975
xieguigang <xie.guigang@gcmodeller.org>
2018-12-31 19:56:51 +0800

    renderer interface

--- renderer.ts
+++ renderer.ts
@@ -146,4 +146,13 @@
 
 interface Renderer {
 
+    strong(text: string): string;  
+    em(text: string): string;   
+    codespan(text: string): string;   
+    del(text: string): string;   
+    text(text: string): string;  
+    image(href: string, title: string, text: string): string;  
+    link(href: string, title: string, text: string): string; 
+    br(): string;
+    
 }
--- textRenderer.ts
+++ textRenderer.ts
@@ -1,31 +1,35 @@
-﻿module textRenderer {
+﻿/**
+ * returns only the textual part of the token
+ * no need for block level renderers
+*/
+class textRenderer implements Renderer {
 
+    public strong(text: string): string {
+        return text;
+    }
 
+    public em(text: string): string {
+        return text;
+    }
+    public codespan(text: string): string {
+        return text;
+    }
+    public del(text: string): string {
+        return text;
+    }
+    public text(text: string): string {
+        return text;
+    }
 
-    /**
-     * TextRenderer
-     * returns only the textual part of the token
-     */
-
-    function TextRenderer() { }
-
-    // no need for block level renderers
-
-    TextRenderer.prototype.strong =
-        TextRenderer.prototype.em =
-        TextRenderer.prototype.codespan =
-        TextRenderer.prototype.del =
-        TextRenderer.prototype.text = function (text) {
-            return text;
-        };
-
-    TextRenderer.prototype.link =
-        TextRenderer.prototype.image = function (href, title, text) {
-            return '' + text;
-        };
-
-    TextRenderer.prototype.br = function () {
-        return '';
+    public image(href: string, title: string, text: string): string {
+        return '' + text;
     };
 
+    public link(href: string, title: string, text: string): string {
+        return '' + text;
+    };
+
+    public br(): string {
+        return '';
+    };
 }
